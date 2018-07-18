264a45866e910703f8fe76440dc1c7639c0e4069
kevinccbsg <kevinccbsg@gmail.com>
2018-07-18 20:47:03 +0200

    First page with style jsx and static assets

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
-
 export default class extends React.Component {
   render() {
-    return (
-      <h1>Hello world!</h1>
-    );
-  }    
+    return <div>
+      <h1>¡Hola Platzi!</h1>
+      <p>Bienvenidos todos al curso de Next.Js</p>
+
+      <img src="/static/platzi-logo.png" alt="Platzi" />
+
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+        :global(div p) {
+          color: green;
+        }
+        img {
+          max-width: 50%;
+          display: block;
+          margin: 0 auto;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          background: white;
+        }
+      `}</style>
+    </div>
+  }
 }
