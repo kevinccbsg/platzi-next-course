2eddbbfb4c92f3c0593d4116af37fce555e93940
kevinccbsg <kevinccbsg@gmail.com>
2018-07-19 00:25:36 +0200

    Refactor channel

--- pages/channel.js
+++ pages/channel.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Layout from '../components/Layout';
+import ChannelGrid from '../components/ChannelGrid';
 
 export default class extends React.Component {
 
@@ -25,25 +27,14 @@ export default class extends React.Component {
   render () {
     const { channel, audioClips, series } = this.props
     return (
-      <div>
-        <header>Podcasts</header>
+      <Layout title="Podcast">
         <h1>{channel.title}</h1>
         {(series.length > 0) && (
           <div>
             <h2>Series</h2>
-            <div className="channels">
-              {series.map((serie) => (
-                <Link href={`/channel?id=${serie.id}`} prefetch>
-                  <a className="channel">
-                    <img src={serie.urls.logo_image.original} alt=""/>
-                    <h2>{serie.title}</h2>
-                  </a>
-                </Link>
-              ))}
-            </div>
+            <ChannelGrid channels={series} />
           </div>
         )}
-
         <h2>Ultimos Podcasts</h2>
         {audioClips.map((clip) => (
           <Link href={`/podcast?id=${clip.id}`}>
@@ -51,50 +42,6 @@ export default class extends React.Component {
           </Link>
         ))}
         <style jsx>{`
-          header {
-            color: #fff;
-            background: #8756ca;
-            padding: 15px;
-            text-align: center;
-          }
-
-          .banner {
-            width: 100%;
-            padding-bottom: 25%;
-            background-position: 50% 50%;
-            background-size: cover;
-            background-color: #aaa;
-          }
-
-          .channels {
-            display: grid;
-            grid-gap: 15px;
-            padding: 15px;
-            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-          }
-          a.channel {
-            display: block;
-            margin-bottom: 0.5em;
-            color: #333;
-            text-decoration: none;
-          }
-          .channel img {
-            border-radius: 3px;
-            box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-            width: 100%;
-          }
-          h1 {
-            font-weight: 600;
-            padding: 15px;
-          }
-          h2 {
-            padding: 5px;
-            font-size: 0.9em;
-            font-weight: 600;
-            margin: 0;
-            text-align: center;
-          }
-
           .podcast {
             display: block;
             text-decoration: none;
@@ -123,7 +70,7 @@ export default class extends React.Component {
             background: white;
           }
         `}</style>
-      </div>
+      </Layout>
     )
   }
 }
