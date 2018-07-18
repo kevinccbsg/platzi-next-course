850870e618c35096ad48b2fcc4bf8b65c909e570
kevinccbsg <kevinccbsg@gmail.com>
2018-07-19 00:27:58 +0200

    Added meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,8 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Podcast | platzi</title>
         </Head>
         <header>
